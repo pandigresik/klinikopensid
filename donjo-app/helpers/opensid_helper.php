6068d4306e61d96d16412dc4fc5fca28104f1a1e
ahmad afandi <ahmad.afandi85@gmail.com>
2025-01-25 12:08:19 +0700

    update versi

--- donjo-app/helpers/opensid_helper.php
+++ donjo-app/helpers/opensid_helper.php
@@ -51,7 +51,7 @@ defined('BASEPATH') || exit('No direct script access allowed');
  * Format => [dua digit tahun dan dua digit bulan].[nomor urut digit beta].[nomor urut digit bugfix]
  * Untuk rilis resmi (tgl 1 tiap bulan) dimulai dari 0 (beta) dan 0 (bugfix)
  */
-define('VERSION', '2409.0.0');
+define('VERSION', '2501.0.0');
 
 /**
  * PREMIUM
